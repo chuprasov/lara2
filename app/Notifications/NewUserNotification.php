7622db158e4691739aab0441fd73a7bdbe5bce79
chuprasov <125488152+chuprasov@users.noreply.github.com>
2024-01-22 14:11:51 +0300

    NewUserNotification

--- app/Notifications/NewUserNotification.php
+++ app/Notifications/NewUserNotification.php
@@ -4,7 +4,7 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Facades\Notification;
+use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
 class NewUserNotification extends Notification implements ShouldQueue
