21d3f3b3facc903daaa7c90b83247032c41fa1eb
chuprasov <125488152+chuprasov@users.noreply.github.com>
2024-02-24 13:41:04 +0300

    API auth sanctum

--- app/Routing/ApiRegistrar.php
+++ app/Routing/ApiRegistrar.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Routing;
 
 use App\Contracts\RouteRegistrar;
-use App\Http\Controllers\Api\ApiProductController;
-use Illuminate\Contracts\Routing\Registrar;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\Routing\Registrar;
+use App\Http\Controllers\Api\ApiAuthController;
+use App\Http\Controllers\Api\ApiProductController;
 
 class ApiRegistrar implements RouteRegistrar
 {
@@ -15,9 +16,19 @@ class ApiRegistrar implements RouteRegistrar
     {
         Route::middleware('api')->prefix('api')
             ->group(function () {
-                Route::get('/products/all', [ApiProductController::class, 'getAll'])->name('api.products.all');
-                Route::get('/products/paginate/{cnt}', [ApiProductController::class, 'getPaginate'])->name('api.products.paginate');
-                Route::get('/products/{product:id}', [ApiProductController::class, 'getOne'])->name('api.products.show');
+                Route::get('/products/all', [ApiProductController::class, 'getAll'])
+                    ->name('api.products.all');
+                Route::get('/products/paginate/{cnt}', [ApiProductController::class, 'getPaginate'])
+                    ->name('api.products.paginate')
+                    ->middleware('auth:sanctum');
+                Route::get('/products/{product:id}', [ApiProductController::class, 'getOne'])
+                    ->name('api.products.show')
+                    ->middleware('auth:sanctum');
+
+                Route::post('register',[ApiAuthController::class,'register']);
+                Route::post('login',[ApiAuthController::class,'login']);
+                Route::delete('logout',[ApiAuthController::class,'logout'])
+                  ->middleware('auth:sanctum');
             });
     }
 }
